186c03e0871b377cd73ca7f3247cf3e73fc2dddf
TakuyaEE <75401433+TakuyaEE@users.noreply.github.com>
2021-01-05 22:30:53 +0900

    a

--- app_search_hellowork.py
+++ app_search_hellowork.py
@@ -146,7 +146,8 @@ def results():
                 for case_count,category_item in enumerate(category_items, 1):
                     if str(category_item) in "disp_inline_block width15em":
                         continue
-                    category = (str(category_item.find('div').text)).replace(' ''　', '')
+                    category = str(category_item.find('div').text)
+                    category = category.translate(str.maketrans({'　': '', ' ': ''}))
                     case_list[case_count] = category
 
                 # 賃金
