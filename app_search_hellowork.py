0f9d4519e9d9b0553c9cca8e55cc766324c43832
TakuyaEE <75401433+TakuyaEE@users.noreply.github.com>
2021-01-06 19:04:11 +0900

    a

--- app_search_hellowork.py
+++ app_search_hellowork.py
@@ -185,10 +185,10 @@ def results():
         df = df.drop(columns=df.columns[1])
         df = df.drop(columns=df.columns[2])
         df = df.drop(columns=df.columns[3])
-        df = df.drop(columns=df.columns[3])
         # df = df.drop(columns=df.columns[3])
-        # df = df.drop(columns=df.columns[3])
-        # df = df.drop(columns=df.columns[5])
+        # df = df.drop(columns=df.columns[4])
+        # df = df.drop(columns=df.columns[4])
+        # df = df.drop(columns=df.columns[4])
         df = df.replace('\n''\t', '', regex=True)
 
         # 列のリネーム
