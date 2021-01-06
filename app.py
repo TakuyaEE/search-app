d2b5e747838c5de414f22fb595fd027f85e2ff53
TakuyaEE <75401433+TakuyaEE@users.noreply.github.com>
2021-01-06 18:09:45 +0900

    a

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, Blueprint,
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-# from app_inquiry import inquiry
+from app_inquiry import inquiry
 from app_search_hellowork import search_hellowork
 
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 # 別モジュールをblueprintで呼び出し
 app.register_blueprint(search_hellowork)
-# app.register_blueprint(inquiry)
+app.register_blueprint(inquiry)
 
 
 # ルーティング処理
@@ -39,6 +39,6 @@ def internal_server_error_html(error):
 
 
 if __name__ == "__main__":
-    app.run()
-    # port = int(os.getenv("PORT", 8000))
-    # app.run(host="0.0.0.0", port=port)
+    # app.run()
+    port = int(os.getenv("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
